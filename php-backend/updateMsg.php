bc345035e8297f0c392c37c635b963e28c4a44e9
keatinge <willy.keatinge@gmail.com>
2016-04-30 12:17:54 -0400

    fixing comment

--- php-backend/updateMsg.php
+++ php-backend/updateMsg.php
@@ -19,9 +19,6 @@ $msg = $_POST['msg'];
 $phone = $_POST['phone'];
 
 
-
-//if we didn't replace anything we have to figure out of its in here
-//if we didnt replace anything it means it's either not in the table or already existing in the table
 //this checks if its in the table right now
 $isIn = $pdo->prepare("SELECT * FROM `messages` WHERE `toNum` = :numToCheck AND `message` = :msgToCheck");
 $isIn->bindParam(":numToCheck", $phone);
